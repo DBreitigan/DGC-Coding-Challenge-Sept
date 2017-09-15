89ff282eea13c71841479628b032d93a1bbf7532
Daniel Breitigan <danthemanb3@gmail.com>
2017-09-14 20:12:27 -0600

    Refactor code to make it more readable and compressed all html requests to a single call

--- index.js
+++ index.js
@@ -9,94 +9,39 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.put('/', function (req, res) {
+  res.send(findTopLayers(req));
 });
 
-app.put('/balanced', function (req, res) {
-
-  var left = "";
-  var right = "";
-
-  var leftValue = req.body.left;
-  while(leftValue != null)
-  {
-    left = leftValue.value + left;
-    leftValue = leftValue.left;
-  }
-
-  var rightValue = req.body.right;
-  while(rightValue != null)
-  {
-    right = right + rightValue.value;
-    rightValue = rightValue.right;
-  }
-
-  var response = left + req.body.value + right;
-  res.send(response);
-});
-
-app.put('/Unbalanced', function (req, res) {
-
-  var left = "";
-  var right = "";
-
-  var leftValue = req.body.left;
-  while(leftValue != null)
-  {
-    left = leftValue.value + left;
-    leftValue = leftValue.left;
-  }
-
-  var rightValue = req.body.right;
-  while(rightValue != null)
-  {
-    right = right + rightValue.value;
-    rightValue = rightValue.right;
-  }
-
-  var response = left + req.body.value + right;
-  res.send(response);
-});
-
-app.put('/SeverelyUnbalanced', function (req, res) {
-
-  var left = "";
-  var right = "";
-
+//Find the top-view of the binary search tree
+function findTopLayers(req) {
   var leftMostCount = 0;
+  var rightMostCount = 0;
+  var left = "";
+  var right = "";
 
   var leftValue = req.body.left;
   while(leftValue != null)
   {
     left = leftValue.value + left;
-    leftMostCount++;
     leftValue = leftValue.left;
+    leftMostCount--;
   }
-
-  var rightMostCount = 0;
 
   var rightValue = req.body.right;
   while(rightValue != null)
   {
     right = right + rightValue.value;
-    rightMostCount++;
     rightValue = rightValue.right;
+    rightMostCount++;
   }
 
   var response = left + req.body.value + right;
+  return checkUnderlyingRoots(req.body, response, leftMostCount, rightMostCount, 0);
+}
 
-  var fullresponse = checkOutliers(req.body, response, (leftMostCount * -1) , rightMostCount, 0)
-
-  res.send(fullresponse);
-});
-
-
-app.listen(3000, function () {
-  console.log("Listening to port 3000!");
-});
-
-function checkOutliers (root, response, leftMostCount, rightMostCount, location) {
+//Check if any roots extend farther out than the top level nodes
+function checkUnderlyingRoots (root, response, leftMostCount, rightMostCount, location) {
   var leftResponse = "";
   var rightResponse = "";
 
@@ -112,16 +57,55 @@ function checkOutliers (root, response, leftMostCount, rightMostCount, location)
     rightMostCount++;
   }
 
-  if(root.left != null)
-  {
-    response = checkOutliers(root.left, response, leftMostCount, rightMostCount, location - 1) + leftResponse;
-  }
-  if(root.right != null)
-  {
-    response = rightResponse + checkOutliers(root.right, response, leftMostCount, rightMostCount, location + 1);
-  }
-
-
+  if(root.left != null) response = checkUnderlyingRoots(root.left, response, leftMostCount, rightMostCount, location - 1) + leftResponse;
+  if(root.right != null) response = rightResponse + checkUnderlyingRoots(root.right, response, leftMostCount, rightMostCount, location + 1);
 
   return  leftResponse + response + rightResponse ;
 }
+
+//Start Application
+app.listen(3000, function () {
+  console.log("Listening to port 3000!");
+});
+
+//EXAMPLE INPUT:
+// {
+//   "value": 10,
+//   "left": {
+//     "value": 9,
+//     "left": {
+//       "value": 7,
+//       "left": null,
+//       "right": null
+//     },
+//     "right": {
+//       "value": 6,
+//       "left": {
+//         "value": 5,
+//         "left": null,
+//         "right": null
+//       },
+//       "right": {
+//         "value": 4,
+//         "left": {
+//           "value": 3,
+//           "left": null,
+//           "right": null
+//         },
+//         "right": {
+//           "value": 2,
+//           "left": null,
+//           "right": null
+//         }
+//       }
+//     }
+//   },
+//   "right": {
+//     "value": 8,
+//     "left": null,
+//     "right": null
+//   }
+// }
+
+//EXPECTED RESPONSE:
+//791082
